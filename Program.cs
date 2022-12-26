4b75e71ac103fca32a903a8fcb2795d6a9fed2ac
Mansur Gafurov <Gafurovmansur1@yandex.ru>
2022-12-26 19:25:16 +0500

    add 2 array

--- Program.cs
+++ Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// Итоговый проект ГАФУРОВ МАНСУР 
+
+string[] array1 = { "sdff", "gdgagG", "RR", "1" };
+string[] array2 = new string[2];
+int j = 0;
 Console.WriteLine("Hello, World!");
