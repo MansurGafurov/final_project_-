0b2d1a70bb02d567d10d41d2ed01582530a29f2d
Mansur Gafurov <Gafurovmansur1@yandex.ru>
2022-12-26 19:26:36 +0500

    add method void CheckElements(string[] array1)

--- Program.cs
+++ Program.cs
@@ -3,4 +3,17 @@
 string[] array1 = { "sdff", "gdgagG", "RR", "1" };
 string[] array2 = new string[2];
 int j = 0;
-Console.WriteLine("Hello, World!");
+
+void CheckElements(string[] array1)
+{
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if (array1[i].Length <= 3)
+        {
+            array2[j] = array1[i];
+            j++;
+        }
+    }
+}
+
+Console.WriteLine();
